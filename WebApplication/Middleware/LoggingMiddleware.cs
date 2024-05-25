f8ff3dc6eca2dc178793b1b1be886fbac8b79e76
Dang Phan Thanh Cong <dangphanthanhcong.work@gmail.com>
2024-05-25 17:18:20 +0700

    17:18, 25/05/2024

--- WebApplication/Middleware/LoggingMiddleware.cs
+++ WebApplication/Middleware/LoggingMiddleware.cs
@@ -18,7 +18,6 @@ namespace WebApplication.Middleware
         public async Task Invoke(HttpContext context)
         {
             var request = context.Request;
-            var bodyStr = "";
 
             // This line allows us to set the reader for the request back at the beginning of its stream.
             request.EnableBuffering();
@@ -27,16 +26,16 @@ namespace WebApplication.Middleware
             var buffer = new byte[Convert.ToInt32(request.ContentLength)];
 
             //...Then we copy the entire request stream into the new buffer.
-            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+            await request.Body.ReadAsync(buffer);
 
             // We convert the byte[] into a string using UTF8 encoding...
-            bodyStr = Encoding.UTF8.GetString(buffer);
+            string? bodyStr = Encoding.UTF8.GetString(buffer);
 
             //..and finally, assign the read body back to the request body, which is allowed because of EnableBuffering()
             request.Body = new MemoryStream(buffer);
 
             // Log the required details
-            var log = $"[{DateTime.Now}]\nSchema:{request.Scheme} \nHost: {request.Host} \nPath: {request.Path} \nQueryString: {request.QueryString} \nRequest Body: {bodyStr}\n";
+            var log = $"[{DateTime.Now}]\nSchema:{request.Scheme} \nHost: {request.Host} \nPath: {request.Path} \nQueryString: {request.QueryString} \nRequest Body: {bodyStr}\n-------\n";
             await File.AppendAllTextAsync("logs.txt", log);
 
             await _next(context);
